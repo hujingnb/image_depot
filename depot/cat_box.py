d8475e145c66a648adf36652d6370f53d3f7d605
hujingnb <hujingnb@qq.com>
2022-11-26 16:48:53 +0800

    记录地址

--- depot/cat_box.py
+++ depot/cat_box.py
@@ -7,6 +7,10 @@ from .base import Depot
 
 
 class CatBox(Depot):
+    """
+    :url: https://catbox.moe/
+    """
+
     @classmethod
     def depot_type(cls) -> DepotType:
         return DepotType.CatBox
